d542613b6073e03f80192827464599220b2d48fe
Travis Nesland <tnesland@gmail.com>
2015-04-15 09:41:41 -0400

    App runs in menu bar when launched.

--- MouseMate/AppDelegate.swift
+++ MouseMate/AppDelegate.swift
@@ -12,10 +12,16 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        let icon = NSImage(named: "statusIcon")
+        icon?.setTemplate(true)
+        statusItem.image = icon
+        statusItem.menu = statusMenu
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
