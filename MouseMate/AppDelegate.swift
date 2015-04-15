dde667b77d8095bd787d550d0f643a5b3ca003ec
Travis Nesland <tnesland@gmail.com>
2015-04-15 08:08:30 -0400

    Initial Commit

---
+++ MouseMate/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MouseMate
+//
+//  Created by Travis Nesland on 4/15/15.
+//  Copyright (c) 2015 Travis Nesland. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
